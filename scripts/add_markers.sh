8f13b510ae025ff3632c0993e99d6d799b2eb2e1
Ning Xu <nxu@umich.edu>
2019-07-31 23:30:57 -0400

    fix: Update map path

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -2,7 +2,7 @@
 cp ../dwa_local_planner_params.yaml /opt/ros/kinetic/share/turtlebot_navigation/param/
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin__p5/src/map/map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin__p5/src/udacity-robond-p5/map/map.yaml" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5 
