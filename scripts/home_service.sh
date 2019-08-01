7bdbde834e1b27c5281b49efe9f65f8c1b958d03
Ning Xu <nxu@umich.edu>
2019-07-31 22:29:54 -0400

    feat: Add map

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+cp ../dwa_local_planner_params.yaml /opt/ros/kinetic/share/turtlebot_navigation/param/
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin__p5/src/map/map.yaml" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5 
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cp ../dwa_local_planner_params.yaml /opt/ros/kinetic/share/turtlebot_navigation/param/
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
